2c1a1518eeac9625d8c7813bb7d6751bf646c737
sakthigowtham <42170124+sakthigowtham@users.noreply.github.com>
2019-03-04 14:04:32 +0530

    Create RPI.java

---
+++ RPI.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class RPI
+{
+public  static void main(String arg[])
+{
+Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+for (int i=1;i<=n;i++)
+{
+System.out.println("Hello");
+}
+}
+}
